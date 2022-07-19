afdfdffea2b52a1b6fe01b56f8e545c2cf4c17b7
TalhaTaskaya <Talha.taskaya.58@outlook.de>
2022-07-19 22:32:07 +0200

    abs. Entfernungen der indices der Klammern eingefügt!!

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,5 @@
+import kotlin.math.abs
+
 fun isAddition(formel: String): Boolean {
     return "+" in formel
 }
@@ -35,30 +37,47 @@ fun anzahlKlammernAuf(formel: String): Int {
 fun anzahlKlammernZu(formel: String): Int {
     return formel.filter { it == ')' }.count()
 }
-// Teilt die Formel in die einzelnen Komponenten durch prüfen wo die erste Klammer schließt und kommt dann zurück -> ersetzt das in substring
-
-// TODO
+// Berechne kleinste Abständen von Listen Werten -- soll nur genutzt werden, wenn listAuf und listZu gleich groß sind.
+fun kleinsterAbstandVonIndices(listAuf:MutableList<String>, listZu:MutableList<String>): MutableList<MutableList<Int>> {
+    var message = ""
+    // Liste mit den Abständen der AUF Klammern zu den ZU Klammern
+    // temp[0] entspricht der ersten AUFGEHENDEN Klammer, die WERTE drinne sind die ABSTÄNDE zu den schließenden Klammern!!
+    var temp = MutableList(listAuf.size){MutableList(listAuf.size){0}}
+    for (i in listAuf.indices){
+        for (j in listZu.indices){
+            temp[i][j] = abs(listAuf[i].toInt() - listZu[j].toInt())
+        }
+    }
+    return temp
+}
+// Prüft in der Formel die passenden indices der Klammern.
 fun teileFormelInKlammern(formel: String) {
     if(formel.contains("(")){
         var temp = formel
-        var subAuf = MutableList<Any>(0){}
-        var subZU = MutableList<Any>(0){}
-        while('(' in temp || ')' in temp){
-            subAuf.add(temp.indexOf("("))
-            subZU.add(temp.indexOf(")"))
-            temp = temp.replace("(","")
-            temp = temp.replace(")","")
-            println(temp)
-            println(subAuf)
-            println(subZU)
+        // In diesen Listen werden alle Indices der Klammern gespeichert.
+        var subAuf = MutableList(anzahlKlammernAuf(formel)){""}
+        var subZU = MutableList(anzahlKlammernZu(formel)){""}
+        // für die In dizes in den Listen -> um Arrayoutofbound zu vermeiden.
+        var pointerA = 0
+        var pointerB = 0
+        // Prüfung der indices
+        for(i in formel.indices){
+            if(formel[i] == '('){
+                subAuf[pointerA] = i.toString()
+                pointerA++
+            }
+            if(formel[i] == ')'){
+                subZU[pointerB] = i.toString()
+                pointerB++
+            }
         }
-
+        // PRINTLN ANWEISUNG ÄNDERN ------------------------------------
+        println( kleinsterAbstandVonIndices(subAuf, subZU))
+        // TODO
     }else{
 
     }
 }
-
-
 
 // Funktion in der alle Komponenten zusammengefügt werden
 fun starteProgram() {
@@ -71,10 +90,13 @@ fun starteProgram() {
     if(isFormelKorrekt(formel)){
         // Der Rechner ist im Stande, Punkt-vor-Strich und Klammern zu erkennen. Darüber hinaus kann er die vier Grundoperationen: Multiplikation, Division, Addition, Subtraktion
         println(prüfeOperationen(formel))
-        // Gib Anzahl Kammern aus
+        // Gib Anzahl Klammern aus
         if(isKlammern(formel)){
             // Prüfe ob die Anzahl auf- und zugehender Klammern gleich ist
-            if(anzahlKlammernAuf(formel) == anzahlKlammernZu(formel)) println("Die Anzahl der Klammern in dieser Formel betraegt ${anzahlKlammernAuf(formel)}")
+            if(anzahlKlammernAuf(formel) == anzahlKlammernZu(formel)){
+                println("Die Anzahl der Klammern in dieser Formel betraegt ${anzahlKlammernAuf(formel)}")
+                // AB HIER WEITER
+            }
             // Bei ungleich -> Neue Formel
             else {
                 println("Die Formel hat eine unterschiedliche Anzahl an KLammen, die auf- und zugehen!!")
@@ -95,10 +117,6 @@ fun main() {
     // (  x1  + 3) + 4/(4+31)
     //    x2   + 4 / ( 4+31)
     // x2 + 4 / x3
-    var a = "((13+3)+3)+4/(4+31)"
-    println(a.replace("(", ""))
+    var formel = "((13+3)+3)+4/(4+31)"
+    teileFormelInKlammern(formel)
 }
-
-
-
-
